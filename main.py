a27f14d4646aadc9c31a4252046bf76b0ad66a1c
ehsang1855 <ehsang1855@gmail.com>
2018-03-16 14:24:46 +0330

    fix some bug and improve code

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import re
 import subprocess
 import os
 import ask_ok
-dir_addr=input("pleas enter directory address :")
-process=subprocess.check_output(["ls",dir_addr])
-for line in process.splitlines():
-    if re.search('(mp4)$|(mkv)$|(MTS)$', str(line)[2:-1]):
-        p2 = subprocess.check_output(['ffprobe', '-v', 'error', '-select_streams', 'v:0', '-show_entries', 'stream=codec_name', '-of','default=noprint_wrappers=1:nokey=1', dir_addr + "/" + str(line)[2:-1]])
-        if ask_ok.ask_ok('codec is ' + str(p2)[2:-3]+ ' Do you convert '+str(line)[2:-1]+' ?(y/n)'):
-            os.system("ffmpeg -i "+dir_addr.replace(' ','\ ') + "/" + str(line)[2:-1]+" -c:v libx265 -c:a copy "+dir_addr.replace(' ','\ ') + "/"+str(line)[2:-5]+"_out.mp4")
 
+while True:
+    dir_add = input("pleas enter directory address :")
+    if os.path.isdir(dir_add):
+        break
+    else:
+        print('no such directory!!Tray again')
 
+process = str(subprocess.check_output(["ls", dir_add]))
+dir_list = process[2:-3].split('\\n')
+for line in dir_list:
+    if re.search('(mp4)$|(mkv)$|(MTS)$', line):
 
+        codec_check_command = ['ffprobe', '-v', 'error', '-select_streams', 'v:0', '-show_entries',
+         'stream=codec_name', '-of', 'default=noprint_wrappers=1:nokey=1', dir_add +"/" + line]
+        codec_check = subprocess.check_output(codec_check_command)
+
+        if ask_ok.ask_ok('codec is ' + str(codec_check)[2:-3] + ' Do you convert '+line+' ?(y/n)'):
+
+            command = "ffmpeg -i "+dir_add+"/"+line+" -f matroska -c:v libx265 -acodec libvorbis "+dir_add+"/"+line+"_x265.mkv"
+            print(command)
+            os.system(command)
